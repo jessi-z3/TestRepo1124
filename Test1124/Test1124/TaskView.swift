ee72719dc9a1b4a2af8b9f18e214c65cc725d701
Jessi Zimmerman <jessi_zimmerman@apple.com>
2024-11-24 12:40:49 -0800

    Added TaskView

---
+++ Test1124/Test1124/TaskView.swift
@@ -0,0 +1,24 @@
+//
+//  TaskView.swift
+//  Test1124
+//
+//  Created by Jessi Zimmerman on 11/24/24.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+//    @StateObject viewModel = TaskViewModel()
+    var body: some View {
+        HStack {
+//            Toggle("Show Completed", isOn: $viewModel.showCompleted)
+//                .padding()
+//            Text(viewModel.text)
+//            Text(viewModel.dueDate ?? "No Due Date")
+        }
+    }
+}
+
+#Preview {
+    TaskView()
+}
